a8a29193c22a3d8a7a006e8a59e4db847498251a
James Colannino <crankycyclops@gmail.com>
2015-04-22 16:30:46 -0700

    added callback for displaying current player and did some refactoring of game object

--- js/game.js
+++ js/game.js
@@ -9,6 +9,42 @@ function Game(options) {
 	// Private: The game board.
 	var board;
 
+	// Optional callback that displays information about the current player.
+	var displayCurrentPlayer = false;
+
+	/*************************************************************************/
+
+	if (typeof(options.width) != 'number' || options.width < 1) {
+		throw "width must be at least 1";
+	}
+
+	if (typeof(options.height) != 'number' || options.height < 1) {
+		throw "height must be at least 1";
+	}
+
+	if (typeof(options.numPlayers) != 'number' || options.numPlayers < 2) {
+		throw "numPlayers must be set and must be an integer greater than or equal to 2";
+	}
+
+	if (typeof(options.boardId) != 'string' || options.boardId.length < 1) {
+		throw "the id of the gameboard's SVG tag must be specified";
+	}
+
+	// Required callback that provides the game with information about a player.
+	if (typeof(options.getPlayerData) != 'function') {
+		throw "getPlayerData must be set and must be a function";
+	}
+
+	if (typeof(options.displayCurrentPlayer) != 'undefined') {
+		if ('function' == typeof(options.displayCurrentPlayer)) {
+			displayCurrentPlayer = options.displayCurrentPlayer;
+		} else {
+			throw "displayCurrentPlayer must be a function";
+		}
+	}
+
+	board = new Board(this, options.boardId, options.width, options.height);
+
 	/*************************************************************************/
 
 	/**
@@ -32,6 +68,31 @@ function Game(options) {
 	/*************************************************************************/
 
 	/**
+	 * Private: notifies the current player that it's their turn.
+	 */
+	var notifyPlayer = function () {
+
+		// If we were given a callback, use it to display information about the
+		// current player
+		if (displayCurrentPlayer) {
+			displayCurrentPlayer(players[curPlayer]);
+		}
+	}
+
+	/*************************************************************************/
+
+	/**
+	 * Private: Increments the current player.
+	 */
+	var togglePlayer = function () {
+
+		curPlayer++;
+		curPlayer %= options.numPlayers;
+	}
+
+	/*************************************************************************/
+
+	/**
 	 * Public: Starts the game.
 	 */
 	this.start = function () {
@@ -39,6 +100,9 @@ function Game(options) {
 		getPlayers();
 		board.init();
 		board.draw();
+
+		// Let the first player know that it's their turn.
+		notifyPlayer();
 	}
 
 	/*************************************************************************/
@@ -56,33 +120,11 @@ function Game(options) {
 
 		// move on to the next player if a square wasn't completed
 		else {
-			curPlayer++;
-			curPlayer %= options.numPlayers;
+			togglePlayer(players[curPlayer]);
 		}
+
+		// Let the current player know that it's their turn.
+		notifyPlayer();
 	}
-
-	/*************************************************************************/
-
-	if (typeof(options.width) != 'number' || options.width < 1) {
-		throw "width must be at least 1";
-	}
-
-	if (typeof(options.height) != 'number' || options.height < 1) {
-		throw "height must be at least 1";
-	}
-
-	if (typeof(options.numPlayers) != 'number' || options.numPlayers < 2) {
-		throw "numPlayers must be set and must be an integer greater than or equal to 2";
-	}
-
-	if (typeof(options.boardId) != 'string' || options.boardId.length < 1) {
-		throw "the id of the gameboard's SVG tag must be specified";
-	}
-
-	if (typeof(options.getPlayerData) != 'function') {
-		throw "getPlayerData must be set and must be a function";
-	}
-
-	board = new Board(this, options.boardId, options.width, options.height);
 }
 
